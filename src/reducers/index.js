733fafd54d8d6a68b769892aee4a627cca66eaeb
Shubham <shubham.sportypi@gmail.com>
2023-04-23 17:10:27 +0530

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import changeTheNumber from "./updown";
+
+import { combineReducers } from "redux";
+
+const reducers = combineReducers(
+    {
+        changeTheNumber
+    }
+);
+
+export default reducers;
